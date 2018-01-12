d4118cb3e53fedf358f89e9fba40de397f84e455
Chris Myers <cmyers78@gmail.com>
2018-01-12 12:13:49 -0500

    codable project. step 1 - using exist JSONSerialization formatting

--- KivaLoan/KivaLoan/KivaLoanTableViewController.swift
+++ KivaLoan/KivaLoan/KivaLoanTableViewController.swift
@@ -18,6 +18,7 @@ class KivaLoanTableViewController: UITableViewController {
 
         tableView.estimatedRowHeight = 92.0
         tableView.rowHeight = UITableViewAutomaticDimension
+        getLatestLoans()
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,35 +38,64 @@ class KivaLoanTableViewController: UITableViewController {
                 print(error)
                 return
             }
+            
+            if let response = response {
+                print(response)
+            }
+            
             if let data = data {
                 self.loans = self.parseJsonData(data : data)
                 OperationQueue.main.addOperation {
                     self.tableView.reloadData()
                 }
             }
-            
         }
         task.resume()
-        
     }
     
-    
+    func parseJsonData(data : Data) -> [Loan] {
+        var loans = [Loan]()
+        
+        do {
+            let jsonResult = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary
+            
+            let jsonLoans = jsonResult?["loans"] as! [AnyObject]
+            
+            for jsonLoan in jsonLoans {
+                var loan = Loan()
+                loan.name = jsonLoan["name"] as! String
+                loan.amount = jsonLoan["loan_amount"] as! Int
+                loan.use = jsonLoan["use"] as! String
+                let location = jsonLoan["location"] as! [String : AnyObject]
+                loan.country = location["country"] as! String
+                loans.append(loan)
+            }
+        } catch {
+            print(error)
+        }
+        
+        return loans
+    }
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // Return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // Return the number of rows
-        return 0
+        return loans.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! KivaLoanTableViewCell
-
+        
+        cell.nameLabel.text = loans[indexPath.row].name
+        cell.countryLabel.text = loans[indexPath.row].country
+        cell.useLabel.text = loans[indexPath.row].use
+        cell.amountLabel.text = ("$\(loans[indexPath.row].amount)")
         // Configure the cell...
 
         return cell
